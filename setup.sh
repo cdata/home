1b0751112ab1609227379f2d9419dc08c31ebdd3
Christopher Joel <chris@cloudflare.com>
2011-02-10 11:59:43 -0800

    Added some vim repos, and a hax0r bash tweak from Jason

--- setup.sh
+++ setup.sh
@@ -12,7 +12,9 @@ nodeRepos=( \
 
 vimRepos=( \
     https://github.com/scrooloose/nerdtree.git \
+    https://github.com/scrooloose/nerdcommenter.git \
     https://github.com/tpope/vim-fugitive.git \
+    https://github.com/mattn/zencoding-vim.git \
 )
 
 function initializePlatform {
@@ -75,20 +77,23 @@ function nodeSetup {
 function vimSetup {
     
     cd ~/.vim
-
-    mkdir doc
-    mkdir plugin
-    mkdir syntax
+    
+    mkdir -p .vim/{doc,plugin,syntax}
     
     ln -s ../Repositories/nerdtree/nerdtree_plugin
-    
+    ln -s ../Repositories/zencoding-vim/autoload
+
     cd plugin
     ln -s ../../Repositories/nerdtree/plugin/NERD_tree.vim
+    ln -s ../../Repositories/nerdcommenter/plugin/NERD_commenter.vim
     ln -s ../../Repositories/vim-fugitive/plugin/fugitive.vim
+    ln -s ../../Repositories/zencoding-vim/plugin/zencoding.vim
 
     cd ../doc
     ln -s ../../Repositories/nerdtree/doc/NERD_tree.txt
+    ln -s ../../Repositories/nerdcommenter/doc/NERD_commenter.txt
     ln -s ../../Repositories/vim-fugitive/doc/fugitive.txt
+    ln -s ../../Repositories/zencoding-vim/doc/zencoding.txt
 
     cd $workingDirectory
 }
